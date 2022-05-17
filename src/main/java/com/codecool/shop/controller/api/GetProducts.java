a43ec2b87ef6b1c692688ab555757b7f903de7f5
dudaskobende <dudaskobende@gmail.com>
2022-05-17 19:46:41 +0200

    Add GetProducts api root

--- src/main/java/com/codecool/shop/controller/api/GetProducts.java
+++ src/main/java/com/codecool/shop/controller/api/GetProducts.java
@@ -1,4 +1,8 @@
 package com.codecool.shop.controller.api;
 
-public class GetProducts {
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(name = "getSelectedProducts", urlPatterns = {"/api/products"}, loadOnStartup = 2)
+public class GetProducts extends HttpServlet {
 }
