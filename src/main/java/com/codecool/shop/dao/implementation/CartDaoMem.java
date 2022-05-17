4f5982fb54e8ef9efce165c2eaa6eccc4c832068
benedekhalaj <bendihalaj@gmail.com>
2022-05-17 14:04:59 +0200

    Add CartDaoMem class

---
+++ src/main/java/com/codecool/shop/dao/implementation/CartDaoMem.java
@@ -0,0 +1,34 @@
+package com.codecool.shop.dao.implementation;
+
+import com.codecool.shop.dao.CartDao;
+import com.codecool.shop.model.Product;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CartDaoMem implements CartDao {
+
+    private Map<Product, Integer> data = new HashMap<>();
+    private static CartDaoMem instance = null;
+
+    private CartDaoMem() {
+
+    }
+
+    public static CartDaoMem getInstance() {
+        if (instance == null) {
+            instance = new CartDaoMem();
+        }
+        return instance;
+    }
+
+    @Override
+    public void add(Product product, Integer quantity) {
+
+    }
+
+    @Override
+    public Map<Product, Integer> getAll() {
+        return null;
+    }
+}
