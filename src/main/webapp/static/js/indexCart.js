e56e1286fd21431f04ee56f3a3cccdb7c6377a69
benedekhalaj <bendihalaj@gmail.com>
2022-05-17 16:05:46 +0200

    Add add-to-cart button event listeners on index.html

--- src/main/webapp/static/js/indexCart.js
+++ src/main/webapp/static/js/indexCart.js
@@ -1,5 +1,13 @@
+import {dataHandler} from "./dataHandler.js";
+
 main();
 
 function main() {
+    const addProductButtons = document.querySelectorAll(".add-product");
+    addProductButtons.forEach((button) => button.addEventListener("click", addProductToCart))
     console.log("indexCart.js");
+}
+
+function addProductToCart(event) {
+    console.log(event);
 }
