f6fca776bc5047d6b1f7d534e36edbb21d0785e1
dudaskobende <dudaskobende@gmail.com>
2022-05-17 13:45:11 +0200

    Refactor SupplierDaoMem

--- src/main/java/com/codecool/shop/dao/implementation/SupplierDaoMem.java
+++ src/main/java/com/codecool/shop/dao/implementation/SupplierDaoMem.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class SupplierDaoMem implements SupplierDao {
 
-    private List<Supplier> data = new ArrayList<>();
+    private final List<Supplier> data = new ArrayList<>();
     private static SupplierDaoMem instance = null;
 
     /* A private Constructor prevents any other class from instantiating.
@@ -41,6 +41,6 @@ public class SupplierDaoMem implements SupplierDao {
 
     @Override
     public List<Supplier> getAll() {
-        return data;
+        return List.copyOf(data);
     }
 }
