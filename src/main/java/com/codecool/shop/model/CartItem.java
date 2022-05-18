43c96154b83ae5543d7bcf1c131bd2687f0271d3
benedekhalaj <bendihalaj@gmail.com>
2022-05-18 14:40:09 +0200

    Add CartItem class

---
+++ src/main/java/com/codecool/shop/model/CartItem.java
@@ -0,0 +1,29 @@
+package com.codecool.shop.model;
+
+public class CartItem {
+
+    private final int productId;
+    private int quantity;
+
+    public CartItem(int productId, int quantity) {
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void increaseQuantity() {
+        quantity++;
+    }
+
+    public void decreaseQuantity() {
+        quantity--;
+    }
+
+}
