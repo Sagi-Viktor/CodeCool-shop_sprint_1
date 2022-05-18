703f1366ad6ca5377b6939ef8ffa28beacdb48f5
benedekhalaj <bendihalaj@gmail.com>
2022-05-18 15:59:37 +0200

    Add cart items in cart.js

--- src/main/webapp/static/js/cart.js
+++ src/main/webapp/static/js/cart.js
@@ -3,10 +3,42 @@ import {dataHandler} from "./dataHandler.js";
 main();
 
 async function main() {
-    const cartItems = await getCartItems();
-    console.log(cartItems);
+    const cart = document.getElementById("cart");
+    const cartItemsData = await getCartItemsData();
+    const cartItems = createCartItemsDiv(cartItemsData);
+    cart.innerHTML = cartItems;
 }
 
-async function getCartItems() {
+async function getCartItemsData() {
     return await dataHandler.getItemsFromCart();
+}
+
+function createCartItemsDiv(cartItems) {
+    let cartItemsDiv = ``;
+    for (let cartItem of cartItems) {
+        const product = cartItem["product"];
+        const supplier = product["supplier"];
+        cartItemsDiv += `
+        <div class="cart-item">
+            <p>${product["name"]}</p>
+            <small>${supplier["name"]}</small>
+            ${createQuantitySelect(cartItem["quantity"])}
+    </div>
+        `;
+    }
+    return cartItemsDiv;
+}
+
+function createQuantitySelect(quantity) {
+    let options = ``;
+    for (let number = 1; number < 11; number++) {
+        options += `
+            <option value="${number}" ${(number === quantity) ? "selected" : ""}>${number}</option>
+        `;
+    }
+    return `
+        <select>
+        ${options}
+    </select>
+    `;
 }
