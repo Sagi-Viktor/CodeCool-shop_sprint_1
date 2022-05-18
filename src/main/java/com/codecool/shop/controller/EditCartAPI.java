83d429492c305641745ea547deb8d8a5ee4b8afb
benedekhalaj <bendihalaj@gmail.com>
2022-05-18 17:01:52 +0200

    Update EditCartAPI.doPost()

--- src/main/java/com/codecool/shop/controller/EditCartAPI.java
+++ src/main/java/com/codecool/shop/controller/EditCartAPI.java
@@ -1,5 +1,14 @@
 package com.codecool.shop.controller;
 
+import com.codecool.shop.dao.CartDao;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.implementation.CartDaoMem;
+import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.model.CartItem;
+import com.codecool.shop.model.Product;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,14 +21,25 @@ import java.io.IOException;
 public class EditCartAPI extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // TODO Refactor this
         BufferedReader bufferedReader = req.getReader();
         StringBuilder content = new StringBuilder();
         String inputLine;
         while ((inputLine = bufferedReader.readLine()) != null) {
             content.append(inputLine);
         }
-        System.out.println(content);
+        String cartItemString = content.toString();
+        JsonObject cartItem = new Gson().fromJson(cartItemString, JsonObject.class);
+        int productId = cartItem.get("productId").getAsInt();
+        int quantity = cartItem.get("quantity").getAsInt();
 
+        CartDao cart = CartDaoMem.getInstance();
+        cart.find(productId).ifPresent(item -> item.setQuantity(quantity));
+
+        var cartItems = cart.getAll();
+
+        var cartItemsJson = new Gson().toJson(cartItems);
+        resp.getWriter().println(cartItemsJson);
     }
 
 }
