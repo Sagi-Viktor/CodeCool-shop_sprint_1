cf9f8c2ed8ca32c518da7886004d797dcb0d91f1
sagi-viktor <sagi.viktor1@gmail.com>
2022-05-19 13:20:20 +0200

    Rewrite PaymentController

--- src/main/java/com/codecool/shop/controller/PaymentController.java
+++ src/main/java/com/codecool/shop/controller/PaymentController.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "payment", urlPatterns = {"/checkout/payment"})
+@WebServlet(name = "payment", urlPatterns = {"/checkout/payment/creditcard", "/checkout/payment/paypal", "/checkout/payment/summary"})
 public class PaymentController extends HttpServlet {
 
     @Override
@@ -19,12 +19,30 @@ public class PaymentController extends HttpServlet {
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
 
         WebContext context = new WebContext(req, resp, req.getServletContext());
-        engine.process("payment.html", context, resp.getWriter());
+
+        String path = req.getServletPath();
+        if (path.contains("paypal")) {
+            engine.process("payment-with-paypal.html", context, resp.getWriter());
+        } if (path.contains("creditcard")) {
+            engine.process("payment-with-creditcard.html", context, resp.getWriter());
+        }
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String cardNumber = req.getParameter("card-num-1") + "-" + req.getParameter("card-num-2") + "-" + req.getParameter("card-num-3") + "-" + req.getParameter("card-num-4");
-        System.out.println(cardNumber);
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+        String path = req.getServletPath();
+        if (path.contains("paypal")) {
+            System.out.println("PayPal username: " +req.getParameter("username"));
+
+        } else if (path.contains("creditcard")) {
+            StringBuilder cardNumber = new StringBuilder();
+            for (int i = 1; i <= 4; i++) {cardNumber.append(req.getParameter("card-num-" + i));}
+            System.out.println("Buyer credit card number: " + cardNumber);
+
+        }
+        System.out.println("Payment Done");
+        engine.process("payment-summary.html", context, resp.getWriter());
     }
 }
