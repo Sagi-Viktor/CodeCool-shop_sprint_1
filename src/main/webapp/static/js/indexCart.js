d5c1124ffff22097f0d9d688b691eebb26a75791
benedekhalaj <bendihalaj@gmail.com>
2022-05-19 19:47:41 +0200

    Remove indexCart.js and fix filtered items add to cart feature

--- src/main/webapp/static/js/indexCart.js
+++ src/main/webapp/static/js/indexCart.js
@@ -1,14 +1,2 @@
-import {dataHandler} from "./dataHandler.js";
 
-main();
 
-function main() {
-    const addProductButtons = document.querySelectorAll(".add-product");
-    addProductButtons.forEach((button) => button.addEventListener("click", addProductToCart))
-    console.log("indexCart.js");
-}
-
-async function addProductToCart(event) {
-    const productId = event.currentTarget.getAttribute("data-id");
-    await dataHandler.addProductToCart(productId);
-}
