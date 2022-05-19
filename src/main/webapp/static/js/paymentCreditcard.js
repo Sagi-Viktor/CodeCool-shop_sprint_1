4cda40cb04464e41d1a8a14d3dac4167e76b89c2
sagi-viktor <sagi.viktor1@gmail.com>
2022-05-19 19:15:56 +0200

    Add payment validation files

---
+++ src/main/webapp/static/js/paymentCreditcard.js
@@ -0,0 +1,21 @@
+
+function init() {
+
+}
+
+function validateCardNumber() {
+}
+
+function ValidateExpirationDate() {
+
+}
+
+function validateCardHolderName() {
+
+}
+
+function validateCVVCode() {
+
+}
+
+init();
