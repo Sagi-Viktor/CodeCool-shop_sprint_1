aea90785320e2624b736d2df0bd942378685cf1a
dudaskobende <dudaskobende@gmail.com>
2022-05-31 08:19:30 +0200

    Add SupplierDaoJdbc.add method

--- src/main/java/com/codecool/shop/dao/implementation/database/SupplierDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/implementation/database/SupplierDaoJdbc.java
@@ -1,10 +1,16 @@
 package com.codecool.shop.dao.implementation.database;
 
 import com.codecool.shop.dao.SupplierDao;
+import com.codecool.shop.model.Product;
 import com.codecool.shop.model.Supplier;
 
 import javax.sql.DataSource;
+import java.sql.Array;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SupplierDaoJdbc implements SupplierDao {
     private final DataSource dataSource;
@@ -23,7 +29,20 @@ public class SupplierDaoJdbc implements SupplierDao {
 
     @Override
     public void add(Supplier supplier) {
+        try (Connection connection = dataSource.getConnection()) {
+            String sqlQuery = "INSERT INTO suppliers(id, supplier_name, description, product_id, product_category_id) " +
+                    "VALUES (?, ?, ?, ?, ?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery);
+            preparedStatement.setInt(1, supplier.getId());
+            preparedStatement.setString(2, supplier.getName());
+            preparedStatement.setString(3, supplier.getDescription());
+            preparedStatement.setArray(4, (Array) supplier.getProductsIds());
+            preparedStatement.setArray(5, (Array) supplier.getProductCategoryIds());
+            preparedStatement.executeUpdate();
 
+        } catch (SQLException throwables) {
+            throw new RuntimeException("Error under add supplier to database: " + supplier, throwables);
+        }
     }
 
     @Override
