799f715b1b296a203110b8e8dfa6d04dbc933e66
Zsezsu <zsezsu6@gmail.com>
2022-06-01 13:12:02 +0200

    Rework find method in SupplierDaoJdbc.java

--- src/main/java/com/codecool/shop/dao/implementation/database/SupplierDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/implementation/database/SupplierDaoJdbc.java
@@ -1,13 +1,11 @@
 package com.codecool.shop.dao.implementation.database;
 
 import com.codecool.shop.dao.SupplierDao;
-import com.codecool.shop.model.Product;
 import com.codecool.shop.model.Supplier;
 
 import javax.sql.DataSource;
 import java.sql.*;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class SupplierDaoJdbc implements SupplierDao {
     private final DataSource dataSource;
@@ -50,11 +48,13 @@ public class SupplierDaoJdbc implements SupplierDao {
             if (!resultSet.next()) {
                 return null;
             }
-//            int id, String name, String description, List<Product> products, List<Integer> productCategoryIds
+            int supplierId = resultSet.getInt(1);
+            String name = resultSet.getString(2);
+            String description = resultSet.getString(3);
+            return new Supplier(name, description, supplierId);
         } catch (SQLException throwables) {
             throw new RuntimeException("Error under get supplier from database. searched id: " + id, throwables);
         }
-        return null;
     }
 
     @Override
